13e6e62bf243f46c984b538a925edbe15004256d
jelambrar96 <jelambrar@gmail.com>
2018-05-29 12:26:48 -0500

    detecta algunas notas

--- aplicacion/app.py
+++ aplicacion/app.py
@@ -2,6 +2,39 @@ import cv2
 import numpy as np 
 import matplotlib.pyplot as plt
 
+wholes_filenames = [
+    'templates/whole/whole_bet.png',
+    'templates/whole/whole_over.png'
+]
+
+quarters_filenames = [
+    'templates/quarter/quarter_bet.png',
+    'templates/quarter/quarter_over.png'
+]
+
+half_filenames = [
+    'templates/half/circle_bet.png',
+    'templates/half/circle_over.png'
+]
+
+bemol_filenames = [
+    'templates/bemol/bemol_bet.png',
+    'templates/bemol/bemol_over.png'
+]
+
+wholes = [cv2.imread(item, cv2.IMREAD_GRAYSCALE) for item in wholes_filenames]
+quartes = [cv2.imread(item, cv2.IMREAD_GRAYSCALE) for item in quarters_filenames]
+halfs = [cv2.imread(item, cv2.IMREAD_GRAYSCALE) for item in half_filenames]
+bemols = [cv2.imread(item, cv2.IMREAD_GRAYSCALE) for item in bemol_filenames]
+
+symbols = {
+    'whole': wholes, 
+    'quater': quartes, 
+    'half': halfs,
+    'bemol': bemols
+}
+
+
 def remove_lines(image):
     pass 
 
@@ -23,7 +56,8 @@ def detect_lines(gray):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray,50,150,apertureSize = 3)
 
-    lines = cv2.HoughLines(edges,1,np.pi/180,200)   
+    lines = cv2.HoughLines(edges,1,np.pi/180,200)  
+    out_lines = []  
     for line in lines:
         for rho,theta in line:
             if np.abs(1.5707963 - theta) < 0.0001 :           
@@ -31,13 +65,64 @@ def detect_lines(gray):
                 b = np.sin(theta)
                 x0 = a*rho
                 y0 = b*rho
-
+                #
                 x3 = int(x0)
                 y3 = int(y0)
                 x4 = int(x0 - w*(-b))
                 y4 = int(y0 - h*(a))
-
+                #
                 cv2.line(img,(x3,y3),(x4,y4),(0,0,255),1)
+                out_lines.append([(x3,y3),(x4,y4)])
+                #
+    return img, out_lines
 
-    return img
+def sort_y(lines):
+    y = [item[0][1] for item in lines]
+    y.sort()
+    yk = [int(sum(y[i:i+2])/2) for i in range(0, len(y), 2)]
+    return [yk[i:i+5] for i in range(0, len(yk), 5)]
 
+def scale(pentalines):
+    avg = []
+    for item in pentalines:
+        dif = [ item[i] - item[i-1] for i in range(1, len(item))]
+        avg.append(int(sum(dif)/(len(item)-1)))
+    return avg 
+
+def match(image, kernels, th = 0.75, size = 1):
+    imgout = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    points = [] 
+    for kern in kernels:
+        hk, wk = kern.shape[::-1]
+        # locations = [] 
+        result = cv2.matchTemplate(image, kern, cv2.TM_CCOEFF_NORMED)
+        loc = np.where( result >= th)
+        pts = zip(*loc[::-1])
+        # print(pts)
+
+        """
+        plt.figure()
+        plt.imshow(result, cmap='gray')
+        plt.show()
+        """ 
+
+        # points = points + pts
+        for item in pts: 
+            # print(item)
+            points.append(item)
+            cv2.rectangle(imgout, item, (item[0] + hk, item[1] + wk), (0,0,255), 2)
+
+    return imgout, points
+
+def detect_symbols(image): 
+    cont = 10
+    for key, value in symbols.items(): 
+    # for key, value in symbols:         
+        out, loc = match(image, value, 0.8, 1)
+        """
+        plt.figure()
+        plt.imshow(out)
+        plt.show()
+        """
+        cv2.imwrite('out/' + key + str(cont) + '.png', out)
+        cont = cont + 1
--- aplicacion/pruebas.py
+++ aplicacion/pruebas.py
@@ -5,23 +5,42 @@ import app
 
 def main():
 
-    path = '/home/jelambrar/Git/FinalProject/aplicacion/demos_musescores/All_Dudes/All_Dudes-001.jpg'
+    # path = '/home/jelambrar/Git/FinalProject/aplicacion/demos_musescores/All_Dudes/All_Dudes-001.jpg'
+    path = '/home/jelambrar/Git/FinalProject/aplicacion/demos_musescores/Triumph/Triumph-001.jpg'
     # path = '/home/jelambrar/Git/SheetVision/resources/samples/sheet.jpg'
     # path = '/home/jelambrar/Git/FinalProject/aplicacion/demos_musescores/Reunion/Reunion-001.jpg'
     image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     image2 = app.threshold(image)
     image3 = app.imgerode(image2)
-    out = app.detect_lines(image)
-    cv2.imwrite('out/lines.jpg',image2)
-    plt.figure()
-    plt.imshow(out)
+    out, lines = app.detect_lines(image3)
+    
+    """
+    cv2.imwrite('out/lines.jpg',out)
+    cv2.imwrite('out/threshold.jpg',image2)
+    """
 
+    """
+    print('lines')
+    for item in lines:
+        print(item)
+    print('\n')
+    """
 
-    plt.figure()
-    plt.imshow(image3, cmap='gray')
+    y = app.sort_y(lines)
+    
+    """ print (y) """
 
-
-    plt.show() 
+    scale = app.scale(y)
+    
+    """ print(scale) """
+    
+    """
+    for item in lines:
+        print(item)
+    """  
+    
+    app.detect_symbols(image)
+    pass 
 
 if __name__ == '__main__':
     main()    
